dceb1fff78d11304301f3a14dce851f5299db458
Louis <louisxiv831@gmail.com>
2025-06-27 02:19:07 +0200

    mo you bitch the file is calles arrays.java

---
+++ glorp/src/arrays.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class arrays {
+  public static void main(String args[]) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    String[] names = new String[5];
+    double[][] marks = new double[5][5];
+    double[] sum = new double[5];
+
+    for (int i = 0; i < names.length; i++) {
+      System.out.println("name ? ");
+      names[i] = scanner.nextLine();
+
+      for (int j = 0; j < 5; j++) {
+        System.out.println("insert five marks ");
+        marks[i][j] = scanner.nextDouble();
+
+        double total = 0;
+
+        total += marks[i][j];
+        sum[i] = total;
+      }
+
+    }
+    // printing
+    for (int i = 0; i < 5; i++) {
+      System.out.print(names[i]);
+      for (int j = 0; j < 5; j++) {
+
+        System.out.print(marks[i][j]);
+      }
+      System.out.println("sum=" + sum[i]);
+    }
+  }
+}
--- glorp/src/ta.java
+++ glorp/src/ta.java
@@ -17,7 +17,6 @@ public static void main (String []args){
             for (int i=1 ; i<100 ; i ++) {
                System.out.println("cock");
                
-           
           }
           scanner.close();
     }
